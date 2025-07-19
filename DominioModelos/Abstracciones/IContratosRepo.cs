89e0af0f2e05856ef9085fe57b0f694f0d760f9e
ShadowLEO1076 <guillermoleoanrdocalvache@gmail.com>
2025-07-18 23:50:23 -0500

    Segundo Commit

--- DominioModelos/Abstracciones/IContratosRepo.cs
+++ DominioModelos/Abstracciones/IContratosRepo.cs
@@ -10,5 +10,10 @@ namespace Dominio.Modelos.Abstracciones
 {
     public interface IContratosRepo : IRepositorio<Contratos>
     {
+       
+        Task<IEnumerable<Contratos>> BuscarPorEmpleadoAsync(string cedula); // Método para buscar contratos por empleado
+        Task<IEnumerable<Contratos>> BuscarPorFechaAsync(DateTime fechaInicio, DateTime fechaFin); // Método para buscar contratos por rango de fechas contratos
+        Task<bool> ExisteContratoPorEmpleadoAsync(string cedula); // Método para verificar si existe un contrato por empleado
+        Task<bool> ExisteContratoPorFechaAsync(DateTime fechaInicio, DateTime fechaFin); // Método para verificar si existe un contrato por rango de fechas
     }
 }
